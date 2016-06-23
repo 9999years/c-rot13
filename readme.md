b0a223f8eed26a69bf1dcccf176027160ac4bdcb
Rebecca Turner <637275@gmail.com>
2016-06-23 18:09:56 -0400

    working beta! maybe ill add piping next

---
+++ readme.md
@@ -0,0 +1,5 @@
+#rot13 for C
+
+It’s simple, and it doesn’t work yet. But it might soon!
+
+Inspired by the lack of a simple rot13 for Windows and my surplus of free time.
--- rot13.c
+++ rot13.c
@@ -1,17 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
-
 int main( int argc, char *argv[] ) {
-	//input string
-	//char *input;
-	printf( "%d\n", argc );
-	//int i = 0;
-	//for( ; i < NELEMS(argv); i++ )
-	//{
-		//input = strcat( input, argv[i] );
-	//}
-	//printf( "%s", input );
-	return 0;
+	//if there aren't any arguments, we have nothing to do
+	if( argc <= 1 )
+	{
+		return 0;
+	}
+	//otherwise...
+	else
+	{
+		//input string, for concatenation
+		//i'll just "waste" like 1kb of memory i guess
+		char input[1024];
+		strcpy( input, "\0" );
+
+		int i = 1;
+		for( ; i < argc; i++ )
+		{
+			//if there was a space in the input
+			if( i > 1 )
+			{
+				//tack on a space
+				strcat ( input, " " );
+			}
+			strcat( input, argv[i] );
+		}
+
+		//input now contains the text to be rot'd
+		//ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+		//NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm
+		i = 0;
+		for( ; i < strlen( input ); i++ )
+		{
+			if( //uppercase, first half
+				( input[i] >= 0x41 && input[i] <= 0x4d ) ||
+				//or lowercase, first half
+				( input[i] >= 0x61 && input[i] <= 0x6d )
+			  )
+			{
+				input[i] += 13;
+			}
+			else if ( //uppercase, second half
+				( input[i] >= 0x4e && input[i] <= 0x5a ) ||
+				//or lowercase, second half
+				( input[i] >= 0x6e && input[i] <= 0x7a )
+				)
+			{
+				input[i] -= 13;
+			}
+		}
+		printf( "%s\n", input );
+		return 0;
+	}
 }
