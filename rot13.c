21ba63ac836f30cdb3ed87966f4af8f1fbd1beeb
Rebecca Turner <637275@gmail.com>
2016-06-23 17:17:28 -0400

    init commit

---
+++ rot13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+
+int main( int argc, char *argv[] ) {
+	//input string
+	//char *input;
+	printf( "%d\n", argc );
+	//int i = 0;
+	//for( ; i < NELEMS(argv); i++ )
+	//{
+		//input = strcat( input, argv[i] );
+	//}
+	//printf( "%s", input );
+	return 0;
+}
